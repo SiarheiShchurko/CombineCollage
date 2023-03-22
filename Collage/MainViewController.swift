ae9cd8786d88d351ce281b7003a70602a3f61c25
SiarheiShchurko <Pridemc@yandex.ru>
2023-03-22 11:39:46 +0300

    no message

--- Collage/MainViewController.swift
+++ Collage/MainViewController.swift
@@ -24,7 +24,6 @@ class MainViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     let collageSize = imagePreview.frame.size
-    
   }
   
   private func updateUI(photos: [UIImage]) {
